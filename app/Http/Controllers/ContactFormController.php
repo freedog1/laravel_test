69577e54d1ab5f29f6a00e09c4a9681304a09fbc
freedog1 <koki375656@gmail.com>
2020-10-17 23:54:38 +0900

    controllwer  show

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -77,6 +77,9 @@ class ContactFormController extends Controller
     public function show($id)
     {
         //
+        $contact = ContactForm::find(id);
+
+        return view('contact.show', compact('contact'));
     }
 
     /**

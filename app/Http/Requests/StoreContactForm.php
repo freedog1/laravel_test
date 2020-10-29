5321faa42c004bd515af6e4d72135d9bcc841e55
freedog1 <koki375656@gmail.com>
2020-10-29 23:56:27 +0900

    バリデーション

--- app/Http/Requests/StoreContactForm.php
+++ app/Http/Requests/StoreContactForm.php
@@ -26,6 +26,13 @@ class StoreContactForm extends FormRequest
         return [
             //
             'your_name' => 'required|string|max:20',
+            'title' => 'required|string|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'url' => 'url|nullable',
+            'gender' => 'required',
+            'age' => 'required',
+            'contact' => 'required|string|max:200',
+            'caution' => 'required|accrpted',
         ];
     }
 }
